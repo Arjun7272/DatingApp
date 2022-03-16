1c2daff28e4a8d0edc0329479e8ad0f1c36346ea
Arjun7272 <Arjungulati72@gmail.com>
2022-03-16 13:45:25 +0530

    End of Section1

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Api.Entiities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set;}
+    }
+}
---
+++ Api/Entiities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace Api.Entiities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+}
